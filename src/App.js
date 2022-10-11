52c0cc69eb8868a110802d26734010af6da0d22a
Hyeong-Joon <gudwns1020@naver.com>
2022-10-11 20:47:05 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   function onClick() {
     setCounter((current) => current + 1);
   }
-  console.log("render");
+  console.log("i run all the time");
+  function iRunOnlyOnce() {
+    console.log("i run only once");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
