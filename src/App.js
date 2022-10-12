061beef7afac327cf9b9f2b0a94427ed3f61e031
Hyeong-Joon <gudwns1020@naver.com>
2022-10-12 23:03:53 +0900

    #7.6 Parameters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route></Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
