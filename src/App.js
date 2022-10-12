e08c7865377fa23c2cb5769f12670b670410a024
Hyeong-Joon <gudwns1020@naver.com>
2022-10-12 14:17:57 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  function onChange(event) {
-    setToDo((current) => (current = event.target.value));
-  }
-  function onSubmit(event) {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  }
-  console.log(toDos);
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="해낼 일 1."
-        />
-        <button>해낼 일 추가</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
